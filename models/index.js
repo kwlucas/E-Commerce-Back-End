04e8104a7b38285d581239e5cfda85656b4b22aa
kwlucas <lucaswkyle@gmail.com>
2022-10-19 11:02:37 -0600

    Formatting adjustments

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const { triggerAsyncId } = require('async_hooks');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -19,12 +18,12 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag
-})
+});
 
 module.exports = {
   Product,
