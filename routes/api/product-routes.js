8f1e7f6d9f1285faf72b8cca2aaa4a14e8ada54a
kwlucas <lucaswkyle@gmail.com>
2022-10-19 11:47:53 -0600

    Created product delete route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,8 +115,18 @@ router.put('/:id', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     // delete one product by its `id` value
+    try {
+        const productData = await Product.destroy({
+            where: { id: req.params.id }
+        });
+        //terenary operator returning 404 if data returns null/empty and 200 if data is present
+        productData ? res.status(200).json(productData) : res.status(404).json({ message: 'No data found!' });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
