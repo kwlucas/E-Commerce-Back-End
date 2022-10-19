1e8bcbfba05fc17a75ae96d8f59615d63a6d00eb
kwlucas <lucaswkyle@gmail.com>
2022-10-19 11:32:06 -0600

    Created category delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,8 +64,18 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     // delete a category by its `id` value
+    try {
+        const categoryData = await Category.destroy({
+            where: { id: req.params.id }
+        });
+        //terenary operator returning 404 if data returns null/empty and 200 if data is present
+        categoryData ? res.status(200).json(categoryData) : res.status(404).json({message: 'No data found!'});
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 module.exports = router;
