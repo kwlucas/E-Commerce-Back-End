388af586bbc1353d5b46ebf52d6f6ff00c07279e
kwlucas <lucaswkyle@gmail.com>
2022-10-19 11:25:52 -0600

    created category post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,8 +33,18 @@ router.get('/:id', async(req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // create a new category
+    try {
+        const newCategory = {
+            category_name: req.body.category_name
+        }
+        const categoryData = await Category.create(newCategory);
+        res.status(200).json(categoryData);
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.put('/:id', (req, res) => {
