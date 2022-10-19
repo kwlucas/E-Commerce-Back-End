31af41641d39d05773a39f9eafc2fff46606e655
kwlucas <lucaswkyle@gmail.com>
2022-10-19 11:29:40 -0600

    created category update route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,8 +47,21 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     // update a category by its `id` value
+    try {
+        const newCategory = {
+            category_name: req.body.category_name
+        }
+        const categoryData = await Category.update(newCategory, {
+            where: { id: req.params.id }
+        });
+        //terenary operator returning 404 if data returns null/empty and 200 if data is present
+        categoryData ? res.status(200).json(categoryData) : res.status(404).json({message: 'No data found!'});
+        
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.delete('/:id', (req, res) => {
