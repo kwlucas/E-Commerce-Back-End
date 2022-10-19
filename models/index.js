8db17020fd897b8783036ba972a05e7da04e169f
kwlucas <lucaswkyle@gmail.com>
2022-10-19 10:57:27 -0600

    Setup Category to Product relationship One to Many

--- models/index.js
+++ models/index.js
@@ -5,8 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
