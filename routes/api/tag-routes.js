e603da137528d8996a7d2c54f88ef0cc8e97f317
kwlucas <lucaswkyle@gmail.com>
2022-10-20 10:29:00 -0600

    Created get all route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,22 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // find all tags
     // be sure to include its associated Product data
+    try {
+        const tagData = await Tag.findAll({
+            include: [{
+                model: Product,
+                through: ProductTag
+            }]
+        });
+        //terenary operator returning 404 if data returns null/empty and 200 if data is present
+        tagData ? res.status(200).json(tagData) : res.status(404).json({ message: 'No data found!' });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
